776663d842d1bd11b1bbd1110fc1cb3fcbd241df
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-08-24 21:37:23 +0300

    used queries to manipulate  data from the database

---
+++ lib/insert.sql
@@ -0,0 +1,21 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate",5,"M","Chocolate","aggressive",1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Rowdy",2,"F","Black","calm",0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Tabitha",4,"F","Brown","calm",1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Sergeant Brown",6,"M","brown","calm",1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mellisa",5,"F","White","aggressive",1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Grinch",1,"M","White","calm",0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Wendy",2,"F","Black","aggressive",0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+(NULL,NULL,NULL,NULL,NULL,NULL);
